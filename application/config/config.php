d23ffc41acfc369b28ee029107a82928efc747a5
modestmaha <modestmaha@gmail.com>
2013-11-22 11:03:59 +0500

    pg database setup

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/LinkedIn/';
+//$config['base_url']	= 'http://localhost/LinkedIn/';
+$config['base_url']	= 'http://shopify-coupons.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
